6de49cdf442212312a75a48c1617a3e504bf8c3f
Kelvinc9654 <KelvinC9654@gmail.com>
2025-11-19 23:52:31 -0500

    changed log levels

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -25,7 +25,7 @@ const SentryTransport = new winston.transports.Console({
 });
 
 const logger = winston.createLogger({
-  level: process.env.LOG_LEVEL || "sily",
+  level: "silly",
   format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
   transports: [
     new winston.transports.Console(),
